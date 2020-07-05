9cfa511b97db8dc1060904159dd8c52fa204a0bd
Kristy Li <kristyli20.0@gmail.com>
2020-07-05 12:45:30 -0400

    Add BusinessesList Screen to render all businesses

--- screens/BusinessesList.js
+++ screens/BusinessesList.js
@@ -1,34 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
-import yelp from '../src/api/yelp';
-import * as Location from 'expo-location';
-import axios from 'axios';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-import Business from './components/Business';
+import React, {useState, useEffect} from 'react'
+import {View, Text, StyleSheet, FlatList} from 'react-native'
+import * as Location from 'expo-location'
+import axios from 'axios'
+import {Avatar, Button, Card, Title, Paragraph} from 'react-native-paper'
+import Business from './components/Business'
 
 class BusinessesList extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      businesses: []
+      businesses: [],
     }
   }
 
   async componentDidMount() {
-    const response = await axios.get('/api/businesses');
-    this.setState({businesses: businesses.data});
+    const response = await axios.get('/api/businesses')
+    this.setState({businesses: businesses.data})
   }
 
   render() {
-    console.log(this.state.businesses)
-      return (
-    <View>
-      <Business />
-    </View>
-  )
+    return (
+      <View>
+        <Business />
+      </View>
+    )
   }
 }
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({})
 
-export default BusinessesList;
+export default BusinessesList
