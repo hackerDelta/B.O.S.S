3b89180343b47c6e97c079d2b48fa3597b4e07ec
Kristy Li <kristyli20.0@gmail.com>
2020-07-06 16:16:06 -0400

    Reformat App Screen

--- App.js
+++ App.js
@@ -1,31 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import BusinessesList from './clients/BusinessesList';
 
 export default function App() {
-  return (
-    <View>
-      {/* <MapView
-        style={{alignSelf: 'stretch', height: 500}}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      /> */}
-      <BusinessesList />
-    </View>
-  );
+  return <BusinessesList />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
