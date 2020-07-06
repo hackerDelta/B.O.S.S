6397181bac4c74b757be4785edc52993624c5f42
Courtney Edwards <courtney.b.edwards95@gmail.com>
2020-07-06 16:43:40 -0400

    feat: HomePage view renders upon app launch

---
+++ client/components/HomePage.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  ImageBackground,
+  TouchableHighlight,
+  SafeAreaView
+} from 'react-native';
+
+const image = require('../../assets/HomePage.png');
+
+export function HomeScreen() {
+  return (
+    <ImageBackground source={image} style={styles.image}>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.appTitle}>WELCOME!</Text>
+        <TouchableHighlight style={styles.button}>
+          <Text>Sign Up</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button}>
+          <Text>Log In</Text>
+        </TouchableHighlight>
+      </SafeAreaView>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  appTitle: {
+    fontSize: 25
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'teal',
+    padding: '5%',
+    borderRadius: 10,
+    width: '50%'
+  },
+  container: {
+    flex: 1,
+    marginTop: '100%',
+    marginBottom: '15%',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  image: {
+    height: '100%',
+    width: '100%'
+  }
+});
