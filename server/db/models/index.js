fea6ba2fb17715dd4f4109ba143b241b18215bd6
Courtney Edwards <courtney.b.edwards95@gmail.com>
2020-07-07 15:27:13 -0400

    chore: update model associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,16 @@
 const db = require('../db');
 const Business = require('./business');
 const Comment = require('./comment');
+const User = require('./user');
 
-Business.hasMany(Comment);
-Comment.belongsTo(Business);
+Business.hasOne(User);
+
+User.hasMany(Business, { through: Comment });
+Business.hasMany(User, { through: Comment });
 
 module.exports = {
   db,
   Business,
   Comment,
+  User
 };
