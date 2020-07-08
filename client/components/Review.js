6124b8507f429d71a7afa146610fa4897d691766
Kristy Li <kristyli20.0@gmail.com>
2020-07-08 12:40:52 -0400

    Feat: Create a Review component

---
+++ client/components/Review.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import StarRating from 'react-native-star-rating';
+
+const Review = ({ review }) => {
+  const { title, comment, stars } = review;
+
+  return (
+    <View style={styles.backgroundStyle}>
+      <Title>{title}</Title>
+      <StarRating
+        disabled={false}
+        emptyStar={'ios-star-outline'}
+        fullStar={'ios-star'}
+        halfStar={'ios-star-half'}
+        iconSet={'Ionicons'}
+        maxStars={5}
+        rating={stars}
+        starSize={20}
+        fullStarColor={'red'}
+      />
+      <Paragraph>{comment}</Paragraph>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  backgroundStyle: {
+    margin: '5%',
+    backgroundColor: '#F0EEEE',
+    textAlign: 'center'
+  },
+  textStyle: {
+    textAlign: 'center'
+  },
+  imageStyle: {
+    flex: 1,
+    margin: '10%',
+    fontSize: 18
+  }
+});
+
+export default Review;
