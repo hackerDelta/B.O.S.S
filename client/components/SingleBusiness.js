de3ef03793e5a14c7289539b5d07ec213dcf13c1
Kristy Li <kristyli20.0@gmail.com>
2020-07-08 16:21:46 -0400

    Chore: Add styles and fix styling

--- client/components/SingleBusiness.js
+++ client/components/SingleBusiness.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { View, StyleSheet, Text, ScrollView } from 'react-native';
-import { Card, Title, Paragraph } from 'react-native-paper';
+import { Card, Title, Paragraph, DataTable } from 'react-native-paper';
 import moment from 'moment';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import Comments from './Comments';
+import { Rating } from 'react-native-elements';
 
 const SingleBusiness = ({ route, navigation }) => {
   const { business } = route.params;
@@ -34,59 +35,72 @@ const SingleBusiness = ({ route, navigation }) => {
   const hoursOutput = isClosed ? (
     <Text>Users report this location has closed.</Text>
   ) : (
-    <View style={{ alignItems: 'center' }}>
-      <Text style={{ fontWeight: 'bold' }}>Location and Hours</Text>
+    <View>
+      <Text style={styles.hourStyle}>Hours</Text>
       {daysOfTheWeek.map((day, index) =>
         !hoursOpened[index] ? (
-          <Text key={day}>{day} Closed</Text>
+          <Text key={day} style={styles.paragraphStyle}>
+            {day} Closed
+          </Text>
         ) : (
           <Text
+            style={styles.paragraphStyle}
             key={day}
           >{`${day} ${hoursOpened[index].start} - ${hoursOpened[index].end}`}</Text>
         )
       )}
     </View>
   );
+  const calculatedTotalRating = comments.length
+    ? comments.reduce(
+        (accumulator, comment) => accumulator + comment.stars,
+        0
+      ) / comments.length
+    : 0;
 
   return (
     <ScrollView>
       <View style={styles.backgroundStyle}>
-        <Title style={styles.textStyle}>{name}</Title>
-        <Paragraph
-          style={styles.textStyle}
-        >{`${address} \n${city}, ${state} ${postalCode} \n ${phone}`}</Paragraph>
-        {hoursOutput}
+        <Title style={styles.titleStyle}>{name}</Title>
+        <Rating
+          type="custom"
+          ratingCount={5}
+          imageSize={20}
+          startingValue={calculatedTotalRating}
+          readonly={true}
+        />
         <Card.Cover style={styles.imageStyle} source={{ uri: `${imageUrl}` }} />
-        <Comments comments={comments} />
-        <MapView
-          style={{
-            alignSelf: 'stretch',
-            height: 500,
-            marginTop: 40,
-            marginLeft: 20,
-            marginRight: 20
-          }}
-          provider={PROVIDER_GOOGLE}
-          showsUserLocation={true}
-          region={{
-            latitude: Number(latitude),
-            longitude: Number(longitude),
-            latitudeDelta: 0.1,
-            longitudeDelta: 0.1
-          }}
-          zoomEnabled={true}
-          scrollEnabled={true}
-          showCompass={true}
-          rotateEnabled={false}
-        >
-          <Marker
-            coordinate={{
+        <View style={styles.container}>
+          <Text style={styles.textStyle}>Location</Text>
+          <Paragraph
+            style={styles.paragraphStyle}
+          >{`${address} \n${city}, ${state} ${postalCode} \n ${phone}`}</Paragraph>
+          <MapView
+            style={styles.mapStyle}
+            provider={PROVIDER_GOOGLE}
+            showsUserLocation={true}
+            region={{
               latitude: Number(latitude),
-              longitude: Number(longitude)
+              longitude: Number(longitude),
+              latitudeDelta: 0.1,
+              longitudeDelta: 0.1
             }}
-            title={name}
-          />
-        </MapView>
+            zoomEnabled={true}
+            scrollEnabled={true}
+            showCompass={true}
+            rotateEnabled={false}
+          >
+            <Marker
+              coordinate={{
+                latitude: Number(latitude),
+                longitude: Number(longitude)
+              }}
+              title={name}
+            />
+          </MapView>
+        </View>
+        {hoursOutput}
+        <Comments comments={comments} />
       </View>
     </ScrollView>
   );
@@ -95,12 +109,45 @@ const SingleBusiness = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   backgroundStyle: {
     margin: '10%',
-    backgroundColor: '#F0EEEE',
+    paddingTop: '10%',
+    paddingBottom: '10%',
+    backgroundColor: 'white',
     borderRadius: 5,
     borderWidth: 1,
     textAlign: 'center'
   },
+  mapStyle: {
+    alignSelf: 'center',
+    height: 200,
+    width: 300,
+    marginLeft: 20,
+    marginRight: 20
+  },
+  starsStyle: {
+    alignSelf: 'center'
+  },
+  container: {
+    flex: 1
+  },
+  titleStyle: {
+    fontSize: 23,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
   textStyle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  hourStyle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: '5%',
+    marginBottom: '2%'
+  },
+  paragraphStyle: {
+    fontSize: 15,
     textAlign: 'center'
   },
   imageStyle: {
