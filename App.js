0ff6b552baa0d6f8761c4b143caf80c8434de51f
Courtney Edwards <courtney.b.edwards95@gmail.com>
2020-07-10 11:25:52 -0400

    style: add auto styling to device status bar

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import HomeScreen from './client/components/HomeScreen';
+import { StatusBar } from 'expo-status-bar';
 import { Router, Stack, Scene } from 'react-native-router-flux';
 import {
   BusinessesList,
   // CommentForm,
+  HomeScreen,
   SignIn,
   SignUp,
   SingleBusiness
@@ -11,15 +12,22 @@ import {
 
 export default function App() {
   return (
-    <Router>
-      <Stack key="root" hideNavBar>
-        <Scene key="home" component={HomeScreen} initial />
-        <Scene key="signin" component={SignIn} title="Signin" />
-        <Scene key="signup" component={SignUp} title="Signup" />
-        <Scene key="businesses" component={BusinessesList} title="Businesses" />
-        <Scene key="business" component={SingleBusiness} title="Business" />
-        {/* <Scene key="commentForm" component={CommentForm} title="commentForm" /> */}
-      </Stack>
-    </Router>
+    <>
+      <StatusBar style="auto" />
+      <Router>
+        <Stack key="root" hideNavBar>
+          <Scene key="home" component={HomeScreen} initial />
+          <Scene key="signin" component={SignIn} title="Signin" />
+          <Scene key="signup" component={SignUp} title="Signup" />
+          <Scene
+            key="businesses"
+            component={BusinessesList}
+            title="Businesses"
+          />
+          <Scene key="business" component={SingleBusiness} title="Business" />
+          {/* <Scene key="commentForm" component={CommentForm} title="commentForm" /> */}
+        </Stack>
+      </Router>
+    </>
   );
 }
