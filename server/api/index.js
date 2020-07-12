d879e4fb77aef428dfe1b84eb6024b2ea561fd23
Courtney Edwards <courtney.b.edwards95@gmail.com>
2020-07-12 19:01:09 -0400

    refactor: partition business routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,22 +1,9 @@
 const express = require('express');
-const { Business, Comment, User } = require('../db/models');
 
 const app = express();
 const PORT = 3001;
 
-app.get('/api/businesses', async (req, res, next) => {
-  try {
-    const businesses = await Business.findAll({
-      include: [
-        { model: Comment, include: { model: User } },
-        { model: User, as: 'owner' }
-      ]
-    });
-
-    res.status(200).json(businesses);
-  } catch (err) {
-    console.log(err);
-  }
-});
+app.use('/api/businesses', require('./businesses'));
+app.use('/api/users', require('./users'));
 
 app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
