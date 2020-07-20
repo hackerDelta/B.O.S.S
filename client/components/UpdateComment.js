66ca4da6d61a2d6732bbcb3726cd52e158d7e528
Kristy Li <kristyli20.0@gmail.com>
2020-07-19 23:31:20 -0400

    Chore: Update key

--- client/components/UpdateComment.js
+++ client/components/UpdateComment.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import Comment from './Comment';
+import uuid from 'react-native-uuid';
 
 const UpdateComment = ({ comment }) => {
   const output = comment ? (
     <View>
-      <Comment key={comment.title} information={comment} type="update" />
+      <Comment key={uuid.v4()} information={comment} type="update" />
     </View>
   ) : null;
 
