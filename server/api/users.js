75c90d35bfb2d930123ae65060f2022f5a9b89fe
Kristy Li <kristyli20.0@gmail.com>
2020-07-19 23:49:22 -0400

    Feat: Add routes and validation to them

--- server/api/users.js
+++ server/api/users.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const { Business, Comment, User } = require('../db/models');
+const {
+  validateAdmin,
+  validateUser,
+  validateUserOrAdmin
+} = require('../utils/permission');
 
 const router = express();
 
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll();
+
     res.json(users);
   } catch (error) {
     next(error);
-  }
-});
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    let user = await User.findOne({ where: { id: req.params.id } });
-    res.json({ user });
-  } catch (err) {
-    next(err);
   }
 });
 
@@ -42,10 +39,49 @@ router.get('/:ownerId', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    const createdOwner = await User.create(req.body);
-    res.status(200).json(createdOwner);
-  } catch (error) {
-    next(error);
+    const newBusiness = req.body;
+    const createdBusiness = await Business.create(newBusiness);
+
+    res.status(201).json(createdBusiness);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const businessObj = req.body;
+  const business = await Business.findByPk(id);
+
+  try {
+    if (business) {
+      await business.update(businessObj);
+
+      res.status(200).send(business);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const userToRemove = await User.findByPk(id);
+
+    if (userToRemove) {
+      const numRows = await userToRemove.destroy();
+
+      if (numRows) {
+        res.sendStatus(204);
+      }
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
