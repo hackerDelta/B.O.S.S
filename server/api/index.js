bda60ef409c9a7c7a872da04530c611bad416754
Courtney Edwards <courtney.b.edwards95@gmail.com>
2020-07-20 12:05:11 -0400

    fix: urlencoded extended options passed in

--- server/api/index.js
+++ server/api/index.js
@@ -10,7 +10,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sessionStore = new SequelizeStore({ db });
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', parameterLimit: 50000 }));
+app.use(
+  express.urlencoded({ limit: '50mb', parameterLimit: 50000, extended: true })
+);
 
 passport.serializeUser((user, done) => done(null, user.id));
 
